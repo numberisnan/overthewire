4f75e6fee7945f8c9ef42e24f028e2be8c7b894c
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2022-08-26 21:30:30 -0400

    Make level.sh create new dir

--- bandit/level.sh
+++ bandit/level.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Format: ./level.sh <level>
 a=$(expr $1 - 1)
+if [ ! -d ./$1 ] ; then
+    mkdir ./$1
+fi
 sshpass -p "$(cat "./$a/pass.txt")" ssh "bandit$1@bandit.labs.overthewire.org" -p 2220
