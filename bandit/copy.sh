25e8173cbb059dc16d6eaf6b496c9508340fa873
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2022-08-28 16:49:04 -0400

    Extend copy script

--- bandit/copy.sh
+++ bandit/copy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# Format: ./copy.sh <level> <filepath>
-a=$(expr $1 - 1)
-sshpass -p "$(cat "./$a/pass.txt")" scp -P 2220 "bandit$1@bandit.labs.overthewire.org:$2" "./$1/"
+# Format: ./copy.sh [-r] <level> <filepath>
+if [ "$1" = "-r" ] ; then
+    shift
+    a=$(expr $1 - 1)
+    sshpass -p "$(cat "./$a/pass.txt")" scp -P 2220 "./$1/$2" "bandit$1@bandit.labs.overthewire.org:/tmp/numberisnan"
+else
+    a=$(expr $1 - 1)
+    sshpass -p "$(cat "./$a/pass.txt")" scp -P 2220 "bandit$1@bandit.labs.overthewire.org:$2" "./$1/"
+fi
