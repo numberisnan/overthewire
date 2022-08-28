24bb2ff4828634c895a5a76b1de595feae3ef0f2
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2022-08-28 16:49:14 -0400

    Lvl 24, 25

---
+++ bandit/23/script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat /etc/bandit_pass/bandit24 > /tmp/apples/pass
---
+++ bandit/24/script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+for a in $(seq 0 9) ; do
+    for b in $(seq 0 9) ; do
+        for c in $(seq 0 9) ; do
+            for d in $(seq 0 9) ; do
+                echo "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ" "$a$b$c$d"
+            done
+        done
+    done
+done
